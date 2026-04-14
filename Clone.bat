8fd8c45506913d4a0418e3364cee7252a4b4cc7f
cchen105 <Chen.Chen@amd.com>
2026-04-14 15:09:18 +0800

    Add clone.bat script

---
+++ Clone.bat
@@ -0,0 +1,4 @@
+git clone https://github.com/chenl2/CppLib
+git clone https://github.com/chenl2/MirBaseLib
+git clone https://github.com/chenl2/LOM2Serv
+git clone https://github.com/chenl2/HpSocketLib
